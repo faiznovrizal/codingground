709666849fd4f896aee58c326e0ae62abbcf78cb
faiznovrizal <faiz.novrizzal@gmail.com>
2017-02-27 09:39:35 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,9 @@
+public class Tugas1{
+
+     public static void main(String []args){
+         String nama = "Faiz Novrizal";
+         String prodi = "TIF";
+         String semester = "6";
+        System.out.println("Nama : " + nama + " Prodi : " + prodi + "Semester : " + semester);
+     }
+}
