ff64b2f92613a9b5bebb1ba2670766f2c1f17bf5
faiznovrizal <faiz.novrizzal@gmail.com>
2017-03-05 21:38:42 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas11/Mahasiswa.java
@@ -0,0 +1,9 @@
+public class Mahasiswa extends BiodataTgs11{
+	String nim = "141240000277";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : 
+		" + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
