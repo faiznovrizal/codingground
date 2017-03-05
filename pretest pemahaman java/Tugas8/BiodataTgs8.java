42aba037ec2b6cf342476e52d20479164860b594
faiznovrizal <faiz.novrizzal@gmail.com>
2017-03-05 21:31:04 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas8/BiodataTgs8.java
@@ -0,0 +1,9 @@
+public class BiodataTgs8{
+	String nama = "Faiz Novrizal";
+	String alamat = "Jepara";
+	int umur = 20;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
